a4229e4ee3b9c109fc19a2f98e7efa436a1bd284
Samantha Bowen <sam@bbowen.net>
2026-01-01 23:08:46 -0800

    Cupl.Enumerables: Add CollectionUtils

---
+++ src/Cupl.Enumerables/CollectionUtils.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Cupl.Enumerables
+{
+	public static class CollectionUtils
+	{
+		[return: MaybeNull]
+		public static TValue GetValueOrDefault<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> source, TKey key)
+		{
+			return source.TryGetValue(key, out var value) ? value : default;
+		}
+
+		public static TValue? GetValueOrNull<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> source, TKey key)
+			where TValue : struct
+		{
+			return source.TryGetValue(key, out var value) ? value : (TValue?)null;
+		}
+
+		public static int? IndexOfOrNull<T>(this IList<T> source, T value)
+		{
+			var index = source.IndexOf(value);
+			return (0 <= index) ? index : (int?)null;
+		}
+
+		[return: MaybeNull]
+		public static T PeekOrDefault<T>(this Queue<T> source)
+		{
+			return source.TryPeek(out var result) ? result : default;
+		}
+
+		public static T? PeekOrNull<T>(this Queue<T> source)
+			where T : struct
+		{
+			return source.TryPeek(out var result) ? result : (T?)null;
+		}
+
+		[return: MaybeNull]
+		public static T DequeueOrDefault<T>(this Queue<T> source)
+		{
+			return source.TryDequeue(out var result) ? result : default;
+		}
+
+		public static T? DequeueOrNull<T>(this Queue<T> source)
+			where T : struct
+		{
+			return source.TryDequeue(out var result) ? result : (T?)null;
+		}
+
+		[return: MaybeNull]
+		public static T PeekOrDefault<T>(this Stack<T> source)
+		{
+			return source.TryPeek(out var result) ? result : default;
+		}
+
+		public static T? PeekOrNull<T>(this Stack<T> source)
+			where T : struct
+		{
+			return source.TryPeek(out var result) ? result : (T?)null;
+		}
+
+		[return: MaybeNull]
+		public static T PopOrDefault<T>(this Stack<T> source)
+		{
+			return source.TryPop(out var result) ? result : default;
+		}
+
+		public static T? PopOrNull<T>(this Stack<T> source)
+			where T : struct
+		{
+			return source.TryPop(out var result) ? result : (T?)null;
+		}
+	}
+}
--- src/Cupl.Enumerables/Readme.md
+++ src/Cupl.Enumerables/Readme.md
@@ -1,3 +1,5 @@
-Cupl.Enumerables is a Cupl library extending Enumerable and Collection types.
+Cupl.Enumerables is a Cupl library extending Enumerable and Collection types, with a focus on working with nullables.
 
-`Cupl.Enumerables.EnumerableUtils` offers extension methods similarly to `System.Linq.Enumerable`, with a focus on working with nullables.
+`Cupl.Enumerables.EnumerableUtils` offers extension methods similarly to `System.Linq.Enumerable`.
+
+`Cupl.Enumerables.CollectionUtils` offers extension methods to various collection types, such as `IReadOnlyDictionary<,>`, `IList<>`, `Queue`, and `Stack`.
